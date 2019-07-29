749e3b6fb1a4f063b7e59d0e79b992dd741b6ce9
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-29 00:21:56 -0500

    working on javascript get names to display in html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,15 +1,38 @@
 console.log("hello world");
 console.log($);
 
+let player1name = "";
+let rpsPlayer2 = "";
+
 $("#player1button").on("click",function(event){
     event.preventDefault();
-    console.log("am lsiterning");
+    console.log("am lsiterning player 1");
 
-    var player1name = $("#player1Name").val().trim();
+   player1name = $("#player1Name").val().trim();
     console.log(player1name);
 
-    var rpsPlayer1 = $("#name1").append(`<h2 id="player1">${player1name}</h2>`);
+    player1NameDisplay();
+    
+    $("#player1Name").val("");
 });
 
-function 
+$("#player2button").on("click",function(event){
+    event.preventDefault();
+    console.log("am lsiterning player 2");
+
+   player2name = $("#player2Name").val().trim();
+    console.log(player1name);
+
+    player2NameDisplay();
+    
+    $("#player2Name").val("");
+});
+
+function player2NameDisplay () {
+    rpsPlayer2 = $("#name2").append(`<div class="palyer2nameDisplay"><h2 id="player2">${player2name}</h2></div>`);
+}
+
+function player1NameDisplay () {
+    rpsPlayer1 = $("#name1").append(`<div class="palyer1nameDisplay"><h2 id="player1">${player1name}</h2></div>`);
+}
 
