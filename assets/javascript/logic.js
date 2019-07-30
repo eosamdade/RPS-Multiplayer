1e6caca2fe67942c83759a6c8214b74068372c6a
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-29 23:43:56 -0500

    chat msg and players are able to join

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,103 +17,135 @@ firebase.initializeApp(firebaseConfig);
 //create a variable to ref database
 var database = firebase.database();
 
-var player1name = "";
-var player2Name = "";
-var player1NewMsg = "";
-var player2NewMsg = "";
-var newMsgP1 = "";
-var newMsgP2 = "";
+// var player1name = "";
+// var player2Name = "";
+// var player1NewMsg = "";
+// var player2NewMsg = "";
+// var newMsgP1 = "";
+// var newMsgP2 = "";
+
+var msgScreen = $("#messages");
+var msgForm = $("#messageForm");
+var msgInput = $("#msgInput");
+var msgBtn = $("#submitBtn");
+var database = firebase.database();
+var msgRef = database.ref("/game/msgs/")
+var id = uuid();
+var name;
+
+//enter click event
+$("#nameBtn").on("click",function(event){
+    event.preventDefault();
+
+    var newPlayer = $("#nameInput").val().trim();
+    console.log(newPlayer);
+    
+
+    if (newPlayer.length < 4) {
+        return alert ("name needs to be more than 4 Characters!")
+    }
+
+    $("#userLoginForm").hide();
+
+    $("#welMsg").append(`<p class="ready">PLAYER : ${$("#nameInput").val()} IS READY ⌚ </p>`)
+
+    $("#name").append((`<p class="ready">${$("#nameInput").val()} ⌚ </p>`))
+    
+    return (name = $("#nameInput").val());
+
+});
 
 
 //enter click event
-$("#player1button").on("click",function(event){
-    event.preventDefault();
-    console.log("am lsiterning player 1");
-
-    player1name = $("#player1Name").val().trim();
-
-    database.ref().child("player 1 data").set({
-        player1name: player1name,
-        player1newMsg: player1NewMsg,
-        // dateAdded: firebase.database.ServerVlue.TIMESTAMP
-    })
-
-    // player1NameDisplay();
-    //displaying players name in rps multiplayer and chatroom
-    $("#name1").append(`<div class="palyer1nameDisplay"><h2 id="player1">${player1name}♫</h2></div>`);
-    $("#name1Msg").append(`<div class="palyer1nameDisplay"><h2 id="player1">${player1name}♫</h2></div>`);
-
-    readyPlayer1();
-
-    $("#player1Name").hide();
-    $("#player1button").hide();
-
-});
-
-
-$("#player2button").on("click",function(event){
-    event.preventDefault();
-    console.log("am lsiterning player 2");
-
-    player2name = $("#player2Name").val().trim();
-
-    database.ref().child("Player 2 data").set({
-        player2name: player2name,
-        player2newMsg: player2NewMsg,
-    })
-
-    // player2NameDisplay();
-    //displaying players name in rps multiplayer and chatroom
-    $("#name2").append(`<div class="palyer2nameDisplay"><h2 id="player2">${player2name}❀</h2></div>`);
-    $("#name2Msg").append(`<div class="palyer2nameDisplay"><h2 id="player2">${player2name}❀</h2></div>`);
-
-    readyPlayer2();
-
-    $("#player2Name").hide();
-    $("#player2button").hide();
+// $("#nameSubBtn").on("submit",function(event){
     
-    $("#welMsg").append(`<h2 class="welMsg">CURRENT PLAYERS</h2>`)
-});
+//     event.preventDefault();
+//     console.log("am lsiterning player 1");
 
-
-database.ref().on("value",function(p1datasnapshot){
-    player1Name = (p1datasnapshot.val().player1name);
-    console.log(p1datasnapshot.val().player1NewMsg);
+//     // console.log( $("#nameInput").val().trim());
     
-},function(errorobject) {
-    console.log("Error handled: " + errorobject.code);
-});
+//     // database.ref().child("player 1 data").set({
+//     //     player1name: player1name,
+//     //     player1newMsg: player1NewMsg,
+//     //     // dateAdded: firebase.database.ServerVlue.TIMESTAMP
+//     // })
 
-database.ref().on("value", function(p2datasnapshot){
-    player2Name = (p2datasnapshot.val().player2name);
-    console.log(p2datasnapshot.val().player2NewMsg);
+//     // // player1NameDisplay();
+//     // //displaying players name in rps multiplayer and chatroom
+//     // $("#name1").append(`<div class="palyer1nameDisplay"><h2 id="player1">${player1name}♫</h2></div>`);
+//     // $("#name1Msg").append(`<div class="palyer1nameDisplay"><h2 id="player1">${player1name}♫</h2></div>`);
 
-},function(errorobject) {
-    console.log("Error handled: " + errorobject.code);
-});
+//     // readyPlayer1();
 
+//     // $("#player1Name").hide();
+//     // $("#player1button").hide();
 
-//showing that players have login successfully
-function readyPlayer1 () {
-    $("#ready1").append(`<p class="ready">PLAYER 1 : ${player1name} IS READY ♫ </p>`)
-}
-
-function readyPlayer2 () {
-    $("#ready2").append(`<p class="ready">PLAYER 2 : ${player2name} IS READY ❀</p>`)
-}
+// });
 
 
+// $("#player2button").on("click",function(event){
+//     event.preventDefault();
+//     console.log("am lsiterning player 2");
+
+//     player2name = $("#player2Name").val().trim();
+
+//     database.ref().child("Player 2 data").set({
+//         player2name: player2name,
+//         player2newMsg: player2NewMsg,
+//     })
+
+//     // player2NameDisplay();
+//     //displaying players name in rps multiplayer and chatroom
+//     $("#name2").append(`<div class="palyer2nameDisplay"><h2 id="player2">${player2name}❀</h2></div>`);
+//     $("#name2Msg").append(`<div class="palyer2nameDisplay"><h2 id="player2">${player2name}❀</h2></div>`);
+
+//     readyPlayer2();
+
+//     $("#player2Name").hide();
+//     $("#player2button").hide();
+    
+//     $("#welMsg").append(`<h2 class="welMsg">CURRENT PLAYERS</h2>`)
+// });
+
+
+// database.ref().on("value",function(p1datasnapshot){
+//     player1Name = (p1datasnapshot.val().player1name);
+//     // console.log(p1datasnapshot.val().player1NewMsg);
+    
+// },function(errorobject) {
+//     console.log("Error handled: " + errorobject.code);
+// });
+
+// database.ref().on("value", function(p2datasnapshot){
+//     player2Name = (p2datasnapshot.val().player2name);
+//     // console.log(p2datasnapshot.val().player2NewMsg);
+
+// },function(errorobject) {
+//     console.log("Error handled: " + errorobject.code);
+// });
+
+
+// //showing that players have login successfully
+// function readyPlayer1 () {
+//     $("#ready1").append(`<p class="ready">PLAYER 1 : ${player1name} IS READY ♫ </p>`)
+// }
+
+// function readyPlayer2 () {
+//     $("#ready2").append(`<p class="ready">PLAYER 2 : ${player2name} IS READY ❀</p>`)
+// }
 
 
 
 
 
 
-//showing the current palyers on load of the App
-// database.ref().orderByChild("dataAdded").limitToLast(1).on("value", function(snapshot) {
-//     $("#name1").text(snapshot.val().player1Name);
-//     $("#name2").text(snapshot.val().player2Name);
-// })
+
+
+// //showing the current palyers on load of the App
+// // database.ref().orderByChild("dataAdded").limitToLast(1).on("value", function(snapshot) {
+// //     $("#name1").text(snapshot.val().player1Name);
+// //     $("#name2").text(snapshot.val().player2Name);
+// // })
 
 
 
@@ -122,65 +154,52 @@ function readyPlayer2 () {
 
 //RPS GAME SESSION
 
+
 //player one rps
 $("#p1Rock").on("click",function(){
-    console.log(`${player1name} Rocks`)
+    console.log(`${name} Rocks`)
 })
 
 $("#p1Paper").on("click",function(){
-    console.log(`${player1name} Papers`)
+    console.log(`${name} Papers`)
 })
 
 $("#p1Scissors").on("click",function(){
-    console.log(`${player1name} Scissors`)
+    console.log(`${name} Scissors`)
 })
-
-//player two rps
-$("#p2Rock").on("click",function(){
-    console.log(`${player2name} Rocks`)
-})
-
-$("#p2Paper").on("click",function(){
-    console.log(`${player2name} Papers`)
-})
-
-$("#p2Scissors").on("click",function(){
-    console.log(`${player2name} Scissors`)
-})
-
 
 
 //MSG APP
 
-$("#player1Msgs").on("click",function(event){
+$("#messageForm").on("submit",function(event){
     event.preventDefault();
-    console.log("ding1");
+    console.log("form submited");
 
-    newMsgP1 = $("#player1MsgText").val().trim();
-    console.log(newMsgP1);
+    var text = $("#msgInput").val().trim();
 
-    postMsgP1()
+    if(!$("#msgInput").val().trim()) return;
 
-    $("#player1MsgText").val("");
-});
+    var msg = {
+        id,
+        name,
+        text,
+    };
 
-$("#player2Msgs").on("click",function(event){
-    event.preventDefault();
-    console.log("ding2");
+    msgRef.push(msg);
 
-    newMsgP2 = $("#player2MsgText").val().trim();
-    console.log(newMsgP2);
+    $("#msgInput").val("");
+})
 
-    postMsgP2();
+function updateMsgs (data) {
+   var { id: userId,name,text} = (data.val());
+   var msg = `<li class=" msg ${id == userId && "my" }">
+   <span>
+       <i class="name">${name} :</i>
+       ${text}
+   </span></li>`
 
-    $("#player2MsgText").val("");
-});
-
-//will it be better as a array?
-function postMsgP1() {
-    $("#newMsgP1").append(`<P class = "messagesItem time">${newMsgP1}</P>`)
+   $("#messages").append(msg);
+   
 }
 
-function postMsgP2() {
-    $("#newMsgP2").append(`<P class = "messagesItem time">${newMsgP2}</P>`)
-}
+msgRef.on("child_added", updateMsgs)
